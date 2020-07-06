684c40a5ec52a729c6d8f8d0437fa63e9efe8c7c
annProg <tecbbs@qq.com>
2020-07-06 12:49:26 +0800

    fix: reboot.js

--- files/js/reboot.js
+++ files/js/reboot.js
@@ -1,4 +1,4 @@
 shell.connect('root@192.168.10.101', 'root');
 dba.rebootClusterFromCompleteOutage('itopMgr')
-var cluster = dba.createCluster('itopMgr');
+var cluster = dba.getCluster('itopMgr');
 cluster.status();
